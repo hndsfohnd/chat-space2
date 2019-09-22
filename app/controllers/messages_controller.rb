71500c72818d789bfc9dcbfbab778af3742f1bd8
hndsfohnd <g532848_nj@yahoo.co.jp>
2019-09-22 16:31:31 +0900

    commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,6 +18,6 @@ end
     params.require(:message).permit(:content, :image).merge(user_id:current_user.id)
   end
   def set_group
-    @group = Group.find(params[:group_id])
+    @group = Group.find_by(params[:group_id])
   end  
 end
