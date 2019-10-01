323ff8bdd63dd6db65c79d5779f080de589bad11
hndsfohnd <g532848_nj@yahoo.co.jp>
2019-10-01 17:50:20 +0900

    first

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     message.image ? image = `<img src="${message.image}">` : image = ""
     var html = `
-    <div class="message" data-id="${message.id}">
+    <div class="contents__bottom5" data-message-id ="${message.id}">
     <div class="contents__bottom1">
       ${message.name}
     <div class="contents__bottom1--data">${message.created_at}</div>
@@ -36,5 +36,37 @@ $(function(){
   .fail(function(){
     alert('エラー');
     })
+    var reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+        var last_message_id = $('.contents__bottom5').last().data("message-id") ; //dataメソッドで.messageにある:last最後のカスタムデータ属性を取得しlast_message_idに代入。
+        // var group_id = $(".group").data("group-id");
+        $.ajax({ //ajax通信で以下のことを行う
+          url: "api/messages", //サーバを指定。今回はapi/message_controllerに処理を飛ばす
+          type: 'get', //メソッドを指定
+          dataType: 'json', //データはjson形式
+          data: {id: last_message_id} //飛ばすデータは先ほど取得したlast_message_id。またparamsとして渡すためlast_idとする。
+        })
+        .done(function (messages) { //通信成功したら、controllerから受け取ったデータ（messages)を引数にとる
+          var insertHTML = '';//追加するHTMLの入れ物を作る
+          messages.forEach(function (message) {//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+          
+            insertHTML = buildHTML(message); 
+            //メッセージが入ったHTMLを取
+            $('.contents__bottom').append(insertHTML);//メッセージを追加
+            })
+          $('.contents__bottom').animate({ scrollTop: $('.contents__bottom')[0].scrollHeight},'fast');//最新のメッセージが一番下に表示されようにスクロールする。
+         console.log(last_message_id)
+              })
+        .fail(function () {
+          alert('自動更新に失敗しました');//ダメだったらアラートを出す
+        });
+
+    };
+
+    };
+
+    setInterval(reloadMessages,4000);
+    });
+
+
   });
-});
